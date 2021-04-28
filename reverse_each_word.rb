06fc832a12a0a7ef76336b25cfefeb6dd04c4a6e
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-04-28 12:56:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(string)
-  new_string=[]
-  string.split(" ").collect do|word|
-    new_string<<word.reverse
-  end
-  new_string.join(" ")
+  string.split(" ").collect{|word| word.reverse}.join(" ")
 end
